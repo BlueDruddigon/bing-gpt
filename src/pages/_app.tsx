2e6316a11aac39f89003576a219a9afda25eef5e
BlueDruddigon <haibuihoang1999@gmail.com>
2023-08-28 23:30:11 +0700

    Additional Feat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,9 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
-// import { NextRouter, useRouter } from 'next/router'
 
 import '@/styles/globals.css'
 
-// const isProd: boolean = process.env.NODE_ENV === 'production'
-
 function MyApp({ Component, pageProps }: AppProps) {
-  // const router: NextRouter = useRouter()
-
-  // useEffect((): void => {
-  //   // Save the location
-  //   const { location } = window
-  //   if (isProd) {
-  //     if (location.href.search(/\d/) > 0) {
-  //       return
-  //     }
-  //     if (location.href.search('.html') < 0) {
-  //       location.replace(`${location.href}.html`)
-  //     }
-  //   }
-  // }, [router.asPath])
-
   return <Component {...pageProps} />
 }
 
